8398f35b64bb3319e061abcdd8b136067ca92909
reed23 <reedcy23@gmail.com>
2015-11-24 17:47:33 -0500

    changes

--- js/script.js
+++ js/script.js
@@ -1,5 +1,31 @@
-/* js file */
 
+
+/* js file */
 (function( window, undefined ){
 
+	//buttons that we will click to
+	//switch content 
+	var buttons = $(".controls a");
+	var divName = $(".box").find("div");
+
+	//on function creates events 
+	buttons.on("click", function(e){
+
+		var className = $(this).data("page");
+		divName.hide();
+
+		$(className).slideDown(1000);
+
+        e.preventDefault();
+
+    });
+
+    divName.hide();
+    $(".p1").slideDown(1000);
+
+
+
 })( window, undefined );
+
+
+
